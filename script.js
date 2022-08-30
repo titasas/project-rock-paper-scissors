dea1f49892aad49e07cf76634df0eb2b8795ad5a
titasas <titas.petrauskas@gmail.com>
2022-08-30 21:40:10 +0300

    feat - Add a function to play one round

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+// Figure out what the computer shows
 function getComputerChoice() {
   let computerChoice = " ";
   const randomNumb = Math.floor(Math.random() * 3);
   switch (randomNumb) {
     case 0:
-      computerChoice = "Rock";
+      computerChoice = "rock";
       break;
     case 1:
-      computerChoice = "Paper";
+      computerChoice = "paper";
       break;
     case 2:
-      computerChoice = "Scissors";
+      computerChoice = "scissors";
       break;
   }
   return computerChoice;
+}
+
+// Decide what the user shows
+let playerChoice = prompt(
+  "What do you want to show - rock, paper, or scissors?"
+).toLowerCase();
+
+// Compare inputs and declare the winner
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a DRAW. Go again.";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    return `You lose! The computer has shown ${computerSelection} and beat your ${playerSelection}`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! The computer has shown ${computerSelection} and lost to your ${playerSelection}`;
+  } else {
+    return `You entered ${playerSelection} which is not a valid option`;
+  }
 }
