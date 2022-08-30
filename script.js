c69806ad8058f101e2daa417123cd0570cf44ecc
titasas <titas.petrauskas@gmail.com>
2022-08-30 20:54:41 +0300

    feat - Add a random computer choice

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+  let computerChoice = " ";
+  const randomNumb = Math.floor(Math.random() * 3);
+  switch (randomNumb) {
+    case 0:
+      computerChoice = "Rock";
+      break;
+    case 1:
+      computerChoice = "Paper";
+      break;
+    case 2:
+      computerChoice = "Scissors";
+      break;
+  }
+  console.log(computerChoice);
+}
+
+getComputerChoice();
