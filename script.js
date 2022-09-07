913ae9b35f743ba9a0fee535484091ac53e8ecfb
titasas <titas.petrauskas@gmail.com>
2022-09-07 20:26:51 +0300

    refactor: :construction: Add an infinite loop to wait for a click
    Apparently this makes the browser go ded lol

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playerPoints = 0;
 let computerPoints = 0;
 let roundsPlayed = 0;
+let updated = false;
 
 const ROCK = document.querySelector("#rock__icon");
 const PAPER = document.querySelector("#paper__icon");
@@ -10,18 +11,21 @@ let playerChoice = " ";
 
 // Get player's choice
 ROCK.addEventListener("click", () => {
+  updated = true;
   if (roundsPlayed === 0) {
     playerChoice = "rock";
     game(playerChoice, getComputerChoice());
   } else return (playerChoice = "rock");
 });
 PAPER.addEventListener("click", () => {
+  updated = true;
   if (roundsPlayed === 0) {
     playerChoice = "paper";
     game(playerChoice, getComputerChoice());
   } else return (playerChoice = "paper");
 });
 SCISSORS.addEventListener("click", () => {
+  updated = true;
   if (roundsPlayed === 0) {
     playerChoice = "scissors";
     game(playerChoice, getComputerChoice());
@@ -79,10 +83,12 @@ function playRound(playerSelection, computerSelection) {
 // Play 5 rounds and declare a winner of the whole game
 function game(playerSelection, computerSelection) {
   for (let i = 0; i < 5; i++) {
+    updated = false;
     console.log(playerSelection);
     roundsPlayed = i;
     // HOW TO MAKE IT WAIT FOR THE NEXT ADD EVENT LISTENER HERE? GOOGLING NOW...
     playRound(playerSelection, computerSelection);
+    while (!updated) {}
   }
   roundsPlayed = 0;
   // for (let i = 1; i <= 5; i++) {
